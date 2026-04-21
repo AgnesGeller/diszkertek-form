9a7b8f66aa4d6850d86f90c8218fe7da06318669
AgnesGeller <agnesgeller@cyberanalys.com>
2026-04-21 13:22:57 +0200

    gmailform added

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
+// STEPS
 const steps = document.querySelectorAll(".form-step");
 const nextBtns = document.querySelectorAll(".next-btn");
 const prevBtns = document.querySelectorAll(".prev-btn");
 
 let currentStep = 0;
 
+
+// STEP MEGJELENÍTÉS
 function showStep(index) {
     steps.forEach((step, i) => {
         step.classList.toggle("active", i === index);
     });
 
-    updateProgress(); // EZ ÚJ
+    updateProgress();
 }
 
+
+// NEXT GOMB + VALIDÁCIÓ
 nextBtns.forEach(btn => {
     btn.addEventListener("click", () => {
+
+        const currentInputs = steps[currentStep].querySelectorAll("input, select");
+
+        let valid = true;
+
+        currentInputs.forEach(input => {
+            if (input.hasAttribute("required") && !input.value) {
+                input.style.border = "2px solid red";
+                valid = false;
+            } else {
+                input.style.border = "";
+            }
+        });
+
+        if (!valid) return;
+
         if (currentStep < steps.length - 1) {
             currentStep++;
             showStep(currentStep);
@@ -21,6 +42,8 @@ nextBtns.forEach(btn => {
     });
 });
 
+
+// PREV GOMB
 prevBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         if (currentStep > 0) {
@@ -30,6 +53,8 @@ prevBtns.forEach(btn => {
     });
 });
 
+
+// HERO GOMB → FORM SCROLL
 function startForm() {
     const form = document.querySelector("#surveyForm");
 
@@ -38,16 +63,13 @@ function startForm() {
     });
 
     form.style.transition = "opacity 0.5s";
-form.style.opacity = 0;
-
-setTimeout(() => {
-    form.style.opacity = 1;
-}, 100);
+    form.style.opacity = 0;
 
     setTimeout(() => {
         form.style.opacity = 1;
-    }, 300);
+    }, 100);
 }
+
 
 // FORM SUBMIT
 document.getElementById("surveyForm").addEventListener("submit", function(e){
@@ -56,15 +78,30 @@ document.getElementById("surveyForm").addEventListener("submit", function(e){
     const data = new FormData(this);
 
     let result = {};
-    data.forEach((value, key) => {
+
+   data.forEach((value, key) => {
+    if (result[key]) {
+        result[key] += ", " + value;
+    } else {
         result[key] = value;
-    });
-
-    console.log(result);
-
-    alert("Sikeres beküldés!");
+    }
 });
 
+    console.log(result);
+    if (!result.igeny) {
+    result.igeny = "nincs megadva";
+}
+
+    emailjs.send("service_gi1vj2r", "template_m0utevq", result)
+.then(function(response) {
+    alert("Sikeresen elküldve!");
+}, function(error) {
+    alert("Hiba történt!");
+});
+});
+
+
+// PROGRESS BAR
 const progressBar = document.getElementById("progressBar");
 const stepIndicators = document.querySelectorAll(".step");
 
